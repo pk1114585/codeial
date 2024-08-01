3a792ffd51551b13b146f2bc9a957caad0a3792d
Pradeep kumar <pk1114585@gmail.com>
2024-08-01 22:33:57 +0530

    add scss

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ const session = require('express-session');
 const passport= require('passport');
 const passportLocal= require('./config/passport-local-strategy');
 const MongoStore = require('connect-mongo');
+const saasMiddleware=require('node-sass-middleware');
+app.use(saasMiddleware({
+    src:'./assets/scss',
+    dest:'./assets/css',
+    debug:true,
+    outputStyle:'extended',
+    prefix:'/css'
+}));
 
 const expresLayout = require('express-ejs-layouts');
 app.use(expresLayout);
