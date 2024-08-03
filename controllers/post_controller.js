41eac6ba48c382e539f8fccdb837410e5b4ba0d7
Pradeep kumar <pk1114585@gmail.com>
2024-08-03 23:31:13 +0530

    Post and Comment deleted function added

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,4 +1,5 @@
 const Post= require('../models/post');
+const Comment= require('../models/comment');
 module.exports.create = async function(req, res){
     try{
         let post = await Post.create({
@@ -25,3 +26,26 @@ module.exports.create = async function(req, res){
     }
   
 }
+
+module.exports.destroy = async function(req, res) {
+    try {
+        let post = await Post.findById(req.params.id);
+
+        // Check if the post belongs to the logged-in user
+        if (post.user == req.user.id) {
+            // Delete the post
+            await Post.deleteOne({ _id: req.params.id });
+
+            // Delete associated comments
+            await Comment.deleteMany({ post: req.params.id });
+
+            return res.redirect('back');
+        } else {
+            return res.redirect('back');
+        }
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send('Internal Server Error');
+    }
+}
+
