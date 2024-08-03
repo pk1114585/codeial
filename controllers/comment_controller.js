04db618c9a28dc94d2c1acfb6fd7679074a7b445
Pradeep kumar <pk1114585@gmail.com>
2024-08-04 00:05:58 +0530

    Post's comment delete function added

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -22,3 +22,31 @@ module.exports.create = async function (req, res) {
         res.status(500).send('Internal Server Error');
     }
 }
+
+module.exports.destroy = async function(req, res) {
+    try {
+        // Find the comment by ID
+        let comment = await Comment.findById(req.params.id);
+
+        // Check if the comment belongs to the logged-in user
+        if (comment.user == req.user.id) {
+            // Get the post ID associated with the comment
+            let postId = comment.post;
+            console.log("PostID = " + postId);
+
+            // Remove the comment
+            await Comment.findByIdAndDelete(req.params.id);
+
+            // Update the post to remove the reference to the deleted comment
+            await Post.findByIdAndUpdate(postId, { $pull: { comments: req.params.id } });
+
+            return res.redirect('back');
+        } else {
+            return res.redirect('back');
+        }
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send('Internal Server Error');
+    }
+}
+
